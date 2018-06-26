ff9a822b0c06df8a3485edd4183e640199e808f4
AutumnCAT <autumncat@zouchenghaode-imac.local>
2018-06-26 14:04:55 +0800

    Initial Commit

---
+++ Snake/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Snake
+//
+//  Created by AutumnCAT on 2018/6/26.
+//  Copyright © 2018年 AutumnCAT. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
